2cad1ffb5d1a32dfcc0c8ed7861e536b1e9a62a5
David Olawuyi <adolawuyi@gmail.com>
2022-04-07 16:58:22 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+  2
+  3 /**
+  4  * main - Entry point
+  5  *
+  6  * Return: 0 after printing the function
+  7  */
+  8 int main(void)
+  9 {
+ 10         putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+ 11         return (0);
+ 12 }
+
