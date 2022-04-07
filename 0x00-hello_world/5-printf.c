4442ace7953cef12d417564c143ddf684921fff6
David Olawuyi <adolawuyi@gmail.com>
2022-04-07 16:09:00 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: return 1 after program execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\");
+	return (0);
+}
+
