694282359441e17ff900c43ea90d6aca7908dc34
David Olawuyi <adolawuyi@gmail.com>
2022-04-07 17:03:03 -0700

    Task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
  #include <stdio.h>
-  2
-  3 /**
-  4  * main - Entry point
-  5  *
-  6  * Return: 0 after printing the function
-  7  */
-  8 int main(void)
-  9 {
- 10         putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
- 11         return (0);
- 12 }
+/**
+ * main - Entry point
+ *
+ *Return: 1 after printing the function
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
 
