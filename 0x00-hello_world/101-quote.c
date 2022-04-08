96e3edc163ecbc7dfbf073e9136bbab821a759be
David Olawuyi <adolawuyi@gmail.com>
2022-04-07 17:06:29 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  *
  *Return: 1 after printing the function
  */
-char main(void)
+int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
 
