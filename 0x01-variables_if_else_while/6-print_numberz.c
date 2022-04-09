354cbfcf89267ca34b0faec5023ad4bc40ee9419
David Olawuyi <adolawuyi@gmail.com>
2022-04-08 20:29:40 -0700

    Task 6 Print Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Print numbers 0 to 9 on one line
+* Return: 0
+*/
+int main(void)
+{
+	char n;
+	for (n = '0'; n <= '9'; ++n)
+	putchar('0' + n);
+
+	putchar('\n');
+
+	return (0);
+}
