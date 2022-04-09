014c35abca7879ea5f53c6516009169aaf99cddf
David Olawuyi <adolawuyi@gmail.com>
2022-04-08 19:52:23 -0700

    Task 3 lower and upper alphabet on the same line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print the lower and UPPERCASE alphabet on
+ *	the same line
+ * Return: 0
+ */
+ int main(void)
+ {
+	int ua;
+
+	for (ua = 0; ua < 26; ++ua)
+	putchar('a' + ua);
+	for (ua = 0; ua < 26; ++ua)
+	putchar('A' + ua);
+
+	putchar('\n');
+
+	return (0);
+}
