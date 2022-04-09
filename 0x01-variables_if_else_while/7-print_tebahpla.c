46520024cb7e36dbeb20d08da056706bd61b4538
David Olawuyi <adolawuyi@gmail.com>
2022-04-09 12:57:58 -0700

    Task 7 lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print lowercase alphabets backwards
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; --n)
+	putchar(n);        
+	putchar('\n');
+	
+	return (0);
+ }
