56bf48b8de093b13c5779b25c4883a99aa05f7f2
David Olawuyi <adolawuyi@gmail.com>
2022-04-13 03:09:45 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description: print alphabets in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ua;
+	for (ua = 'a'; ua <= 'z'; ua++)
+	putchar(ua);
+	putchar('\n');
+}
