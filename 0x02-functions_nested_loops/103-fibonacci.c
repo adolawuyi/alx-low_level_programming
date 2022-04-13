6795ae0b14e2a89ca07b8361c14aa7e5d9243a79
David Olawuyi <adolawuyi@gmail.com>
2022-04-13 16:29:12 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,18 +9,16 @@ int main(void)
 	unsigned long fibs = 0, fibs1 = 1, fibsum;
 	float tot_sum;
 
-	while(1)
+	while (1)
 	{
 		fibsum = fibs + fibs1;
 		if (fibsum > 4000000)
 			break;
-		
 		if ((fibsum % 2) == 0)
 		tot_sum += fibsum;
-		
 		fibs = fibs1;
 		fibs1 = fibsum;
 	}
-	printf("%f\n", tot_sum);
+	printf("%.0f\n", tot_sum);
 	return (0);
 }
