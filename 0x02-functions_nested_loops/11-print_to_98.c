2d8c965c290620c9bfe828c17b94145c117ca405
David Olawuyi <adolawuyi@gmail.com>
2022-04-13 07:33:16 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,28 @@
  */
 void print_to_98(int n)
 {
+	int i
+
 	if (n >= 98)
 	{
-	while ( n > 98)
-		printf("%d ", n++);
-		printf("%d\n", n);
+		for (i = n; i > 97; i--)
+		{
+		printf("%d ", i);
+		if (i != 98)
+		{
+		printf(", ");
+		}
+		}
 	}
 	else
 	{
-	while ( n < 98)
-		printf("%d ", n++);
-		printf("%d\n", n);
+		for (i = n; i < 99; i++)
+		{
+		printf("%d ", i);
+		if (i != 98)
+		{
+		printf(", ");
+		}
+		}
 	}
 }
