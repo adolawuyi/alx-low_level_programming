6f08f436ba2241c2ecef8fd0c13f55ba2e9dcbe9
David Olawuyi <adolawuyi@gmail.com>
2022-04-14 17:39:47 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * more_numbers - print numbers from 0 to 14 10x
  *
@@ -7,22 +8,22 @@
 
 void more_numbers(void)
 {
-	int a, b, c;
+int a, b, c;
 
-	for (c = 0; c < 10; c++)
-	{
-		for (a = 0; a <= 1; a++)
-		{
-			for (b = 0; b <= 9; b++)
-			{
-				if (!(a == 1 && b >= 5))
-				{
-					if (i)
-						_putchar(a + '0');
-					_putchar(b + '0');
-				}
-			}
-		}
-		_putchar('\n');
-	}
+for (c = 0; c < 10; c++)
+{
+for (a = 0; a <= 1; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+if (!(a == 1 && b >= 5))
+{
+if (i)
+_putchar(a + '0');
+_putchar(b + '0');
+}
+}
+}
+_putchar('\n');
+}
 }
