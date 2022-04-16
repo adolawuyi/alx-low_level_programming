d9ae3143ec27ca953b8c75f8816e1cd4c7cbca68
David Olawuyi <adolawuyi@gmail.com>
2022-04-15 18:55:57 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,19 +3,19 @@
 /**
  * print_numbers - print numbers from zero to nine
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
 
-int i;
+	int i;
 
-i = 0;
+	i = 0;
 
-for (i = 0; 1 < 10; i++)
-
-_putchar(i + '0');
-
-_putchar ('\n');
+	for (i = 0; 1 < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar ('\n');
 }
